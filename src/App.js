c625b5324f6dab99d07074bb0b9ec6d6eb3ed28e
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-01 18:24:45 +0300

    theme change added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 const App = () => {
-  return <div className='app'></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <main className="content"></main>
+          </div>;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 };
 
 export default App;
