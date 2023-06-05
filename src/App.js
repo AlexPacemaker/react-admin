0016707c859aabd02612401ae03b49d7499dacce
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-05 16:04:05 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-// import Dashboard from "./scenes/dashboard";
+import Dashboard from "./scenes/dashboard";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Сontacts from "./scenes/contacts";
@@ -14,7 +15,6 @@ import Sidebar from "./scenes/global/Sidebar";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 // import Calendar from "./scenes/calendar";
-// import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -27,9 +27,9 @@ const App = () => {
           <Sidebar />
           <main className='content'>
             <Topbar />
-            {/* <Routes>
+            <Routes>
               <Route path='/' element={<Dashboard />} />
-              <Route path='/team' element={<Team />} />
+              {/* <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Сontacts />} />
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
@@ -38,8 +38,8 @@ const App = () => {
               <Route path='/line' element={<Line />} />
               <Route path='/faq' element={<FAQ />} />
               <Route path='/geography' element={<Geography />} />
-              <Route path='/calendar' element={<Calendar />} />
-            </Routes> */}
+              <Route path='/calendar' element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
         ;
