c5e6be5fe0516ff3a810f012814db9c6d56e058a
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-06 15:00:51 +0300

    invoices added

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -5,7 +5,7 @@ import { tokens } from "../../theme";
 import { mockDataContacts } from "../../data/mockData";
 import Header from "../../Components/Header";
 
-const Team = () => {
+const Contacts = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -74,4 +74,4 @@ const Team = () => {
   );
 };
 
-export default Team;
+export default Contacts;
