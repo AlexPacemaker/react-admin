288d7a420c959cb7c18ed1cc37c7cef0328ee14a
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-06 13:05:18 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Сontacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -29,8 +29,8 @@ const App = () => {
             <Topbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team />} />
-              <Route path='/contacts' element={<Сontacts />} />
+              <Route path='/team' element={<Team />} />
+              {/* <Route path='/contacts' element={<Сontacts />} />
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
               <Route path='/bar' element={<Bar />} />
