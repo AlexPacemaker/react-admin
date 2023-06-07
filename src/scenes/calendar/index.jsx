ab4f9494e9861d6b77768ffd06c08986b48fc57f
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-06-07 14:10:23 +0300

    calendar init

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+const Calendar = () => {
+  return <div>Calendar</div>;
+};
+
+export default Calendar;
